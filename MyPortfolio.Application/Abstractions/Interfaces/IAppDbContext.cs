3432a78c432a881850379352e341834b4d803bdf
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-01-26 15:02:45 +0500

    add-some services

---
+++ MyPortfolio.Application/Abstractions/Interfaces/IAppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Application.Abstractions.Interfaces
+{
+    public interface IAppDbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<UserLanguage> UserLanguages { get; set; }
+        DbSet<Certificate> Certificates { get; set; }
+        DbSet<Experience> Experiences { get; set; }
+        DbSet<Language> Languages { get; set; }
+        DbSet<Project> Projects { get; set; }
+        DbSet<Skill> Skills { get; set; }
+        DbSet<Social> Socials { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
