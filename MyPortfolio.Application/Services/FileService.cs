095b12a0c1dd0b51c823e8106bb8f38ff344b03b
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-02-02 00:14:44 +0500

    add

--- MyPortfolio.Application/Services/FileService.cs
+++ MyPortfolio.Application/Services/FileService.cs
@@ -14,11 +14,9 @@ namespace MyPortfolio.Application.Services
         public FileService()
         {
             // Define the base directory for storing files
-            /*_filesDirectory = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            _filesDirectory = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
                                                 ? Path.Combine(Directory.GetCurrentDirectory(), "..", "MyPortfolio.Application", "Files")
-                                                : Path.Combine("/root/MyPortfolio", "Files");*/
-
-            _filesDirectory = Path.Combine("/root/MyPortfolio", "Files");
+                                                : Path.Combine("/app/Files");
 
             // Ensure the directory exists
             Directory.CreateDirectory(_filesDirectory);
