d0db41ee8fe3e8ef6f975e2c9239607d20d33eb1
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-03-01 18:45:37 +0500

    add migration

---
+++ MyPortfolio.Infrastructure/Migrations/20240301134503_Initial2_Migration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial2_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Skills",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Skills");
+        }
+    }
+}
